bf53f4f484ef74030d8cb649a2d34c7f035eada8
Rahul Dit <65885548+ZoltarThunderbird315@users.noreply.github.com>
2021-05-10 12:46:14 +0530

    Add files via upload

---
+++ reversing_a_linkedlist.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+struct Node* head;
+
+void insert(int data, int n)
+{
+	Node* temp1 = new Node();
+	temp1->data = data;
+	temp1->next = NULL;
+	
+	if(n == 1)
+	{
+		temp1->next = head;
+		head = temp1;
+		return;
+	}
+	
+	Node* temp2 = new Node();
+	temp2 = head;
+	for(int i=0; i<n-2; i++)
+	{
+		temp2 = temp2->next;
+	}
+	
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+
+}
+void Print()
+{
+	Node* temp = new Node();
+	temp = head;
+	while(temp != NULL)
+	{
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	
+	cout<<"\n";
+}
+
+void Reverse()
+{
+	Node *current, *prev, *next;
+	current = head;
+	prev = NULL;
+	
+	while(current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+		
+	}
+	
+	head = prev;
+}
+
+int main()
+{
+	head = NULL; //empty list
+	insert(1,1);
+	insert(2,2);
+	insert(3,3);
+	insert(5,3);
+	
+	Print();
+	
+	Reverse();
+	
+	Print();
+	
+	
+	return 0;
+}
